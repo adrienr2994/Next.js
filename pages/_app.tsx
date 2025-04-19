e45d39183c180beb96787ec3376a7eb33a195075
adrienr2994 <148509502+adrienr2994@users.noreply.github.com>
2025-04-18 21:28:30 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,12 @@
+// Run this in a browser console (e.g., via Replit)
+document.body.onkeydown = () => {
+  const chars = "01ABCDEFGH%^&*";
+  let hackerText = "";
+  for (let i = 0; i < 100; i++) {
+    hackerText += chars[Math.floor(Math.random() * chars.length)];
+  }
+  document.body.innerHTML = `<pre>${hackerText}</pre>`;
+};
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
